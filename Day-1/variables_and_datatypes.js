c2f7f0c5525e5184007c5cefce5c495272a51868
Abdul Kareem <abdulkareem6190@gmail.com>
2024-07-13 13:45:02 +0530

    add variables and datatypes demonstration

---
+++ Day-1/variables_and_datatypes.js
@@ -0,0 +1,40 @@
+//  VARIABLE DECLARATION
+// var a = 10;
+// console.log(a);
+
+// let b = 'hello world';
+// console.log(b);
+
+// //  CONSTANT DECLARATION
+// const c = true;
+// console.log(c);
+
+// DATA TYPES
+// let age = 17;
+// let language = 'JavaScript';
+// const eligibleToVote = false;
+// const user = {
+//   name: 'Rohit Sharma',
+//   age: 36,
+//   phone: '8764892455',
+//   email: 'rohitsharma264@gmail.com',
+// };
+
+// const fruits = ['apple', 'banana', 'mango', 'orange'];
+
+// console.log(typeof age);
+// console.log(typeof language);
+// console.log(typeof eligibleToVote);
+// console.log(typeof user);
+// console.log(typeof fruits);
+
+// REASSIGNING VARIABLES
+const language = 'Python';
+console.log(language);
+language = 'JavaScript';
+console.log(language);
+
+// Summary
+// var -> redeclare ✅, reassign ✅, hoisting ✅
+// let -> redeclare ❌, reassign ✅, hoisting ❌ (BLOCK SCOPE)
+// const -> redeclare ❌, reassign ❌, hoisting ❌ (BLOCK SCOPE)
