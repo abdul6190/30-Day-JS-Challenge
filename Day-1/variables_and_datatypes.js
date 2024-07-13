34855cf816ae92e805787b624343558d45043d69
Abdul Kareem <abdulkareem6190@gmail.com>
2024-07-13 17:06:58 +0530

    update variable names

--- Day-1/variables_and_datatypes.js
+++ Day-1/variables_and_datatypes.js
@@ -1,38 +1,38 @@
 //  VARIABLE DECLARATION
-// var a = 10;
-// console.log(a);
+var a = 10;
+console.log(a);
 
-// let b = 'hello world';
-// console.log(b);
+let b = 'hello world';
+console.log(b);
 
 // //  CONSTANT DECLARATION
-// const c = true;
-// console.log(c);
+const c = true;
+console.log(c);
 
 // DATA TYPES
-// let age = 17;
-// let language = 'JavaScript';
-// const eligibleToVote = false;
-// const user = {
-//   name: 'Rohit Sharma',
-//   age: 36,
-//   phone: '8764892455',
-//   email: 'rohitsharma264@gmail.com',
-// };
+let age = 17;
+let language = 'JavaScript';
+const eligibleToVote = false;
+const user = {
+  name: 'Rohit Sharma',
+  age: 36,
+  phone: '8764892455',
+  email: 'rohitsharma264@gmail.com',
+};
 
-// const fruits = ['apple', 'banana', 'mango', 'orange'];
+const fruits = ['apple', 'banana', 'mango', 'orange'];
 
-// console.log(typeof age);
-// console.log(typeof language);
-// console.log(typeof eligibleToVote);
-// console.log(typeof user);
-// console.log(typeof fruits);
+console.log(typeof age);
+console.log(typeof language);
+console.log(typeof eligibleToVote);
+console.log(typeof user);
+console.log(typeof fruits);
 
 // REASSIGNING VARIABLES
-// let language = 'Python';
-// console.log(language);
-// language = 'JavaScript';
-// console.log(language);
+let popularLanguage = 'Python';
+console.log(popularLanguage);
+popularLanguage = 'JavaScript';
+console.log(popularLanguage);
 
 // REASSIGNING CONST
 const color = 'Red';
